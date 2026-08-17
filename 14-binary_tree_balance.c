973b611c5f994377270388daa72e4c42d92bf26f
14835-create <14835@holbertonstudents.com>
2026-08-17 11:40:40 +0000

    error

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -1,6 +1,26 @@
 #include "binary_trees.h"
 
 /**
+ * height - measures height binary tree
+ * @tree: pointer to root node
+ *
+ * Return: height otherwise NULL
+ */
+
+static size_t height(const binary_tree_t *tree)
+{
+	size_t left, right;
+
+	if (tree == NULL)
+		return (0);
+
+	left = height(tree->left);
+	right = height(tree->right);
+
+	return ((left > right ? left : right) + 1);
+}
+
+/**
  * binary_tree_balance - measure balance factor of binary tree
  * @tree: pointer to root node of tree
  *
@@ -12,5 +32,5 @@ int binary_tree_balance(const binary_tree_t *tree)
 	if (tree == NULL)
 		return (0);
 
-	return (binary_tree_height(tree->left) - binary_tree_height(tree->right));
+	return ((int)height(tree->left) - (int)height(tree->right));
 }
